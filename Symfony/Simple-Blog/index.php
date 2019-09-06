d66df4b956c9198a88ad832d92dd57dc4e4a4853
raynaldmo <raynaldmo@gmail.com>
2019-09-06 13:31:57 -0700

    Simple Blog application v2
    * Separate application logic from presentation
    * Add template file

--- Symfony/Simple-Blog/index.php
+++ Symfony/Simple-Blog/index.php
@@ -3,27 +3,15 @@
 $connection = new PDO("mysql:host=localhost;dbname=blog", 'test', 'test');
 
 $result = $connection->query('SELECT post_id, post_title FROM posts');
-?>
 
-<!DOCTYPE html>
-<html>
-<head>
-  <title>List of Posts</title>
-</head>
-<body>
-<h1>List of Posts</h1>
-<ul>
-  <?php while ($row = $result->fetch(PDO::FETCH_ASSOC)): ?>
-    <li>
-      <a href="/show.php?id=<?= $row['post_id'] ?>">
-        <?= $row['post_title'] ?>
-      </a>
-    </li>
-  <?php endwhile ?>
-</ul>
-</body>
-</html>
 
-<?php
+$posts = [];
+
+while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+  $posts[] = $row;
+}
+
 $connection = null;
-?>
+
+// include the HTML presentation code
+require_once 'templates/list.php';
