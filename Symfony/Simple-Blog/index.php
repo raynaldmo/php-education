9e0b75e96c8a9baad128bc5de8d385b69708d3ad
raynaldmo <raynaldmo@gmail.com>
2019-09-07 20:25:48 -0700

    Simple Blog application v4
    * Re-factor to add front-end controller
    * Add .htaccess to do simple URL re-routing

--- Symfony/Simple-Blog/index.php
+++ Symfony/Simple-Blog/index.php
@@ -1,10 +1,23 @@
 <?php
-// index.php - controller
+// index.php -  Front controller
 ini_set('display_errors', '1');
 
 
+// load and initialize any global libraries
 require_once 'model.php';
-$posts = get_all_posts();
+require_once 'controllers.php';
 
-// include the HTML presentation code
-require_once 'templates/list.php';
+// route the request internally
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+// $uri = '/' . basename($uri);
+
+if ( '/' === $uri) {
+  list_action();
+}
+elseif ( (strpos($uri, '/show') !== FALSE) && isset($_GET['id']) ) {
+  show_action($_GET['id']);
+}
+else {
+  header('HTTP/1.1 404 Not Found');
+  echo '<html><body><h1>Page Not Found</h1></body></html>';
+}
